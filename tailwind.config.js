dc8ab8c9318a749c03ffb5a1542c6258e5752bf6
Soom_Coin <nawar.alsafadi.0@gmail.com>
2026-04-22 22:23:50 +0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [],
+  safelist: [{ pattern: /./ }],
+  theme: {
+    extend: {
+      colors: { 
+        gold: '#D4AF37',
+        'dark-gold': '#B8941F',
+        'bg-dark': '#0A0A0A'
+      }
+    },
+  },
+  plugins: [],
+}
